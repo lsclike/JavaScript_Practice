a64d7b8e2a599daa213ab1fb5d0d1921d5c1bfa8
Master_Cloud <lsclike2016@hotmail.com>
2018-11-20 22:34:59 +1100

    first

---
+++ expression/express1.js
@@ -0,0 +1,21 @@
+const foo = function test1(b) { return typeof(b) };
+let foo3 = foo;
+
+
+
+var test = function () {};
+
+console.log(f1)
+function f1(foo) {
+    return foo
+}
+
+if (NaN){
+    console.log('sdh')
+}
+const foo2 = (c) => { return typeof(c()) };
+console.log(foo2(()=> {return 10000}));
+
+
+
+
---
+++ expression/hoisted.js
@@ -0,0 +1,20 @@
+var test = function () {
+    console.log('what ?')
+};
+// compare with test()
+console.log(test);
+
+
+result = map(f, [1, 2, 3]);
+console.log(result)
+function map(f, a) {
+    var result = []; // Create a new Array
+    var i; // Declare variable
+    for (i = 0; i != a.length; i++)
+        result[i] = f(a[i]);
+    return result;
+}
+var f = function(x) {
+    return x * x * x;
+}
+
