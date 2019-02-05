f730568579655f2fb4ba8448e525c5f24f40c981
Master_Cloud <lsclike2016@hotmail.com>
2019-02-05 16:39:05 +1100

    prototype inheritant

--- expression/playThisKeyWord.js
+++ expression/playThisKeyWord.js
@@ -10,11 +10,20 @@ let dog = {
         speak
     }
 };
-console.log(dog.rest.speak());
+// console.log(dog.rest.speak());
 
 //the demonstration of this keyword for arrow functions
 function anyMulti(){
     console.log(this.numbers.map( n => n * this.multi))
 }
 
-anyMulti({numbers: [1, 2, 3], multi: 3});
+anyMulti.call({numbers: [1, 2, 3], multi: 3});
+function returnTest(){
+    console.log('return test');
+    let result = 9;
+    return result
+}
+let arrowTest = () => 3;
+
+testResult = arrowTest();
+console.log(returnTest())
--- expression/prototype.js
+++ expression/prototype.js
@@ -1,15 +1,31 @@
-function Person(name) {
-    this.name = name;
+function Person(firstName, lastName, dob) {
+    this.firstName = firstName;
+    this.lastName = lastName;
     this.fullNa = () => {
         return 'hello ' + 'world'
     };
-    // console.log(this)
+   this.birthday = new Date(dob);
+    // this.calculateAge = function(){
+    //    const diff = Date.now() - this.birthday.getTime();
+    //    const ageDate = new Date(diff);
+    //    return Math.abs(ageDate.getUTCFullYear() - 1970);
+    // }
 }
+
+Person.prototype.calculateAge = function(){
+    const diff = Date.now() - this.birthday.getTime();
+    const ageDate = new Date(diff);
+    return Math.abs(ageDate.getUTCFullYear() - 1970);
+};
+
+Person.prototype.fullName = function(){
+    return `${this.firstName} ${this.lastName}`
+};
 
 // the built-in function in Person can override function in prototype
 // Person.prototype.fullName = () => 'This';
-Object.prototype.fullName = () => 'This from Object';
-// Person.lastName = 'like Class Variable';
-Function.prototype.lastName = 'like Class Variable';
-test_brad = new Person('Brad');
+Object.prototype.fullName = () => 'fullName from Object';
+Function.prototype.lastName = 'this lastName from function prototype';
+test_brad = new Person('Brad', 'Peter', '9-10-1993');
+// console.log(test_brad.calculateAge(test_brad.birthday));
 console.log(test_brad.fullName());
