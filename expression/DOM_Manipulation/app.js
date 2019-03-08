75929436ed22bd7a74f1eef9a5a1f26842cfeac8
Master_Cloud <lsclike2016@hotmail.com>
2019-03-08 23:29:42 +1100

    DOM 2

--- expression/DOM_Manipulation/app.js
+++ expression/DOM_Manipulation/app.js
@@ -1,10 +1,32 @@
 // single selector
-let test = document.getElementById('task-title');
-let querySelector = document.querySelector('.card-title');
-document.querySelector('li').style.background = 'red ';
-document.querySelector('ul li').style.background = 'blue';
-document.querySelector('li:nth-child(3)').style.background = 'red';
-test.style.background = '#333';
+// let test = document.getElementById('task-title');
+// let querySelector = document.querySelector('.card-title');
+// document.querySelector('li').style.background = 'red ';
+// document.querySelectorAll('ul li').style.background = 'red ';
+// document.querySelector('li:nth-child(3)').style.background = 'red';
+// test.style.background = '#333';
 
-const items = document.getElementsByClassName('collection-item');
-items[0].style.background = 'black';
+//get from global content
+// const items = document.getElementsByClassName('collection-item');
+// items[0].style.background = 'black';
+//since htmlCollection is not Array
+const listItem = document.querySelector('ul').getElementsByClassName('collection-item');
+// convert collection to array
+const arrayListItem = Array.from(listItem);
+arrayListItem.forEach( (li, index) =>
+{
+    console.log(li.className);
+    li.textContent = String(index);
+});
+
+const nodeList = document.querySelectorAll('li.collection-item');
+const liOdd = document.querySelectorAll('po li:nth-child(odd)');
+const liEven = document.querySelectorAll('li:nth-child(even)');
+
+liOdd.forEach(li => {
+    li.style.background = "red";
+});
+
+// liEven.forEach(li => {
+//     li.style.background = "blue"
+// });
