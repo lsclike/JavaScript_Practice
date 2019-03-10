69dd966b799ef057e2feb9004bd4850a6ffb0deb
Master_Cloud <lsclike2016@hotmail.com>
2019-03-10 23:26:06 +1100

    event listener

--- expression/DOM_Manipulation/app.js
+++ expression/DOM_Manipulation/app.js
@@ -31,12 +31,53 @@
 //     li.style.background = "blue"
 // });
 
-let val;
-const list = document.querySelector('ul.collection');
-const listItem = document.querySelectorAll('li.collection-item:first');
+// let val;
+// const list = document.querySelector('ul.collection');
+// const listItem = document.querySelectorAll('li.collection-item');
 
 // Get child nodes
-val = list.childNodes;
+// val = list.childNodes;
 
 // Get children element nodes
-val = list.children[1].nodeName;
+// val = list.children[1].nodeName;
+
+// Create Element
+// const li = document.createElement('ul');
+
+// Add Class
+// li.className = 'collection-item';
+// Add id
+// li.id = 'new-item';
+// Add content inside the node
+// li.appendChild(document.createTextNode('Hello world'));
+// const link = document.createElement('a');
+// link.className = 'delete-item secondary-content';
+// link.innerHTML = '<i class="fa fa-remove"></i>';
+// li.appendChild(link);
+// Append li as child to ul
+// document.querySelector('ul.collection').appendChild(li);
+// console.log();
+
+// Replace Elements
+
+// Create Elements
+// const newHeading = document.createElement('h2');
+// newHeading.id = 'task-title';
+// newHeading.appendChild(document.createTextNode('Task List1'));
+// Get old heanding
+// const oldHeading = document.getElementById('task-title');
+// Get parents
+// const cardAction = document.querySelector('.card-action');
+// const cardAction = oldHeading.parentElement;
+// cardAction.replaceChild(newHeading, oldHeading);
+//Remove
+// const lis = document.querySelectorAll('li');
+// const list = document.querySelector('ul');
+
+document.querySelector('.clear-tasks').addEventListener('click', onClick);
+
+function onClick(e) {
+    console.log('Hello World');
+    // let test = e;
+    // console.log(e);
+}
